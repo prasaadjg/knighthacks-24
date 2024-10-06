30e1089894e0200e0f65869be51a2d246590e02c
prasaadjg <prasaadjg@hotmail.com>
2024-10-06 07:01:09 -0400

    create user on sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,10 @@ import { api, HydrateClient } from "~/trpc/server";
 import UserProfile from "./_components/UserProfile";
 import { getAuth } from "@clerk/nextjs/server";
 import { users } from "~/server/db/schema";
+import useUserIdStore from "~/hooks/userIdStore";
 
 export default async function Home() {
+  const { userId } = useUserIdStore();
 
   return (
     <HydrateClient>
