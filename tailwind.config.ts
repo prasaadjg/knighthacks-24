9774040b77faea3145ba338df691fe0b195a26e8
astrid <astrid.wimmer08@gmail.com>
2024-10-06 09:28:30 -0400

    added the css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,21 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
-
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-      },
+      colors: {
+        green1: '#485648',
+        // titleGreen: '#B1DD9E',
+        green2: '#102820',
+        green3: '#315E26',
+        brown1: '#caba9c',
+        brown2: '#8a6240',
+        brown3: '#4d2d18'
+      }
     },
   },
   plugins: [],
-} satisfies Config;
+}
